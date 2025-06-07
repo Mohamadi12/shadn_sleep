2eb38390d1d8823c101c045b1ebd8116fb6adb1f
Nana Mohamadi <adilnana78@gmail.com>
2025-06-07 17:06:28 +0000

    Build About and Contact pages, implement Footer and Guest layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import Guest from '@/components/Guest';
+import { currentUser } from '@clerk/nextjs/server';
 
-const HomePage = () => {
+
+const HomePage = async () => {
+    const user = await currentUser();
+  if (!user) {
+    return <Guest />;
+  }
   return (
     <div>
       <h1 className="text-3xl font-bold mb-4">Sleep Tracker</h1>
